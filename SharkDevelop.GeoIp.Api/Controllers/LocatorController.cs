fb07b71d20fbb23245dc679d631b62ee9c9fc428
Wanl23 <ivan.leschinsky@yandex.ru>
2018-03-31 21:54:35 +0500

    Fixed problem with the test

--- SharkDevelop.GeoIp.Api/Controllers/LocatorController.cs
+++ SharkDevelop.GeoIp.Api/Controllers/LocatorController.cs
@@ -28,7 +28,7 @@ namespace SharkDevelop.GeoIp.Api.Controllers
         /// </summary>
         /// <param name="ipAddress">Приведенный IP</param>
         /// <returns>Имя Странны</returns>
-        [Route("LocateCountry")]
+        [Route("LocateCountry/{ipAddress}")]
         [HttpGet]
         public async Task<IHttpActionResult> LocateCountryAsync(string ipAddress)
         {
